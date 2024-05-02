1b4d12606bd246c82ef81717e6f58f9814664327
Kent <kentfranciskalaw@gmail.com>
2024-05-03 00:43:28 +0800

    navigation bar

--- lib/Main App/home_page.dart
+++ lib/Main App/home_page.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
  
  class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,30 +14,42 @@ import 'package:google_fonts/google_fonts.dart';
 
 class _HomePageState extends State<HomePage> {
 
+  int _selectedIndex = 0;
+
   final user = FirebaseAuth.instance.currentUser!;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Signed in as: ' + user.email!),
-          MaterialButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            color: Colors.black,
-            child: Text('Sign out',
-            style: GoogleFonts.poppins(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 12,
+      bottomNavigationBar: Container(
+        color: Color.fromARGB(234, 234, 234, 234),
+        child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0,vertical: 15),
+        child: GNav(
+          backgroundColor: const Color.fromARGB(234, 234, 234, 234),
+          color: Color.fromARGB(133, 41, 41, 41),
+          activeColor: Color.fromARGB(255, 24, 24, 24),
+          tabBackgroundColor: Color.fromARGB(234, 184, 184, 184),
+          gap: 10,
+          padding: EdgeInsets.all(20),
+          tabs: const [
+            GButton(icon: Icons.home,
+            text: 'Home',),
+            GButton(icon: Icons.favorite_border,
+            text: 'Favorites'),
+            GButton(icon: Icons.search,
+            text: 'Search'),
+            GButton(icon: Icons.person,
+            text: 'Profile'),
+            ],
+            selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+          ),
             ),
-            
-            ),
-          ), 
-        ],
-      )),
+      ),
     );
   }
 }
